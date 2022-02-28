7409ba0cc134dcbaefdf2aec17df0a646913d98b
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-02-28 20:27:40 +0200

    made planet page responsive

--- js/content/planets.js
+++ js/content/planets.js
@@ -1,4 +1,4 @@
-const plannets = [
+const planets = [
   {
     id: 0,
     name: "Mars",
@@ -6,7 +6,7 @@ const plannets = [
       "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
     distance: "225 MIL. km",
     travelTime: "9 months",
-    url: "../../assets/image-mars.png",
+    url: "../assets/destination/image-mars.png",
   },
 
   {
@@ -16,7 +16,7 @@ const plannets = [
       "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
     distance: "628 MIL. km",
     travelTime: "3 years",
-    url: "../../assets/image-europa.png",
+    url: "../assets/destination/image-europa.png",
   },
 
   {
@@ -26,7 +26,7 @@ const plannets = [
       "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
     distance: "1.6 BIL. km",
     travelTime: "7 years",
-    url: "../../assets/image-titan.png",
+    url: "../assets/destination/image-titan.png",
   },
 
   {
@@ -36,6 +36,6 @@ const plannets = [
       "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
     distance: "384,400 km",
     travelTime: "3 days",
-    url: "../../assets/image-moon.png",
+    url: "../assets/destination/image-moon.png",
   },
 ];
