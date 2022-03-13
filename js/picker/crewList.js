2b645a9523f8dd456f5dd5f4d8c768ecd298dce4
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-03-13 18:09:54 +0200

    changed crew member picker

--- js/picker/crewList.js
+++ js/picker/crewList.js
@@ -1,4 +1,4 @@
-const crewList = document.getElementsByClassName("member__list")[0].children[0].children;
+const crewList = document.getElementsByClassName("member__list")[0].children[0];
 let currentMember = null;
 
 const memberImg = document.getElementsByClassName("member__img")[0].children[0];
@@ -6,25 +6,30 @@ const memberName = document.getElementsByClassName("member__name")[0];
 const memberDescr = document.getElementsByClassName("member__descr")[0];
 const memberRole = document.getElementsByClassName("member__role")[0];
 
-for (let i = 0; i < crewList.length; i++) {
-  const member = crewList[i];
+function setMember(member) {
+  memberImg.src = member.url;
+  memberName.textContent = member.name;
+  memberDescr.textContent = member.description;
+  memberRole.textContent = member.role;
+}
 
-  for (let j = 0; j < crew.length; j++) {
-    if (crew[j].role.toLowerCase() === member.getAttribute('data-role').toLowerCase()) {
-      crewList[i].onclick = () => {
+for (let i = 0; i < crew.length; i++) {
+  const member = document.createElement("li");
+  member.setAttribute("data-role", crew[i].role.toLowerCase());
+  crewList.appendChild(member);
 
-        console.log("click")
-        if (currentMember) {
-            currentMember.classList.remove("active-member");
-        }
-        currentMember = member;
-        currentMember.classList.add("active-member");
-
-        memberImg.src = crew[j].url;
-        memberName.textContent = crew[j].name;
-        memberDescr.textContent = crew[j].description;
-        memberRole.textContent = crew[j].role;
-      };
+  member.onclick = () => {
+    console.log("click");
+    if (currentMember) {
+      currentMember.classList.remove("active-member");
     }
-  }
+    currentMember = member;
+    currentMember.classList.add("active-member");
+
+    setMember(crew[i]);
+  };
+}
+
+if (!currentMember) {
+  setMember(crew[0]);
 }
